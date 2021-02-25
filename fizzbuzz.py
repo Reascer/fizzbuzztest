e0edda70f1af1f5a0d00933f98bc808d2486402a
Reascer <48482533+Reascer@users.noreply.github.com>
2021-02-25 17:41:08 +0100

    refacto and pass ( no joke here )

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,8 @@
 def FizzBuzz(numbre):
     if numbre <= 0:
         raise ValueError
+    if numbre % 3 == 0 and numbre % 5 == 0:
+        return "fizzbuzz"
     if numbre % 3 == 0:
         return "fizz"
     if numbre % 5 == 0:
