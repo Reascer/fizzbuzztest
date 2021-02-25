d2da79c589755cbc7d2cedf8ef65b73bc0f6bc75
Reascer <48482533+Reascer@users.noreply.github.com>
2021-02-25 17:33:11 +0100

    le test for multiple of 5 is écrit butt it sadly fail ....

--- case_test.py
+++ case_test.py
@@ -14,6 +14,11 @@ class Casetest(unittest.TestCase):
         exp = "fizz"
         actual = FizzBuzz(18)
         self.assertEqual(exp, actual, "Should be fizz")
+    
+    def test_should_return_fizz_if_numbre_is_multple_of_5(self):
+        exp = "buzz"
+        actual = FizzBuzz(20)
+        self.assertEqual(exp, actual, "Should be buzz")
 
 if __name__ == '__main__':
     unittest.main()
