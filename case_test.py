eb3430c8dff9fb6b35a014120f02731a416c1591
Reascer <48482533+Reascer@users.noreply.github.com>
2021-02-25 17:27:43 +0100

    test for multiple of trois is écrit my frend :U

--- case_test.py
+++ case_test.py
@@ -10,5 +10,10 @@ class Casetest(unittest.TestCase):
         with self.assertRaises(ValueError):
             FizzBuzz(-5)
 
+    def test_should_return_fizz_if_numbre_is_multple_of_3(self):
+        exp = "fizz"
+        actual = FizzBuzz(18)
+        self.assertEqual(exp, actual, "Should be fizz")
+
 if __name__ == '__main__':
     unittest.main()
--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,5 @@
 def FizzBuzz(numbre):
     if numbre <= 0:
         raise ValueError
+    return ">___<"
     
